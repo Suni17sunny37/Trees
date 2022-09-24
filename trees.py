a93a340a46c601e47b1504dc6514a4c1183093cd
Sunil Kumar Reddy <108225478+Suni17sunny37@users.noreply.github.com>
2022-09-24 08:52:54 +0530

    Update trees.py

--- trees.py
+++ trees.py
@@ -9,14 +9,14 @@ class Node:
     def inorder(self,root):
         if root:
             self.inorder(root.left)
-            print(root.data)
+            print(root.data,end=" ")
             self.inorder(root.right)
 
 #PREORDER
 
     def preorder(self,root):
         if root:
-            print(root.data)
+            print(root.data,end=" ")
             self.preorder(root.left)
             self.preorder(root.right)
 
@@ -25,14 +25,14 @@ class Node:
         if root:
             self.postorder(root.left)
             self.postorder(root.right)
-            print(root.data)
+            print(root.data,end=" ")
 
 #NORMAL TREE
 
     def display(self):
         if self.left:
             self.left.display()
-        print(self.data)
+        print(self.data,end=" ")
         if self.right:
             self.right.display()
 
@@ -43,7 +43,14 @@ root.right=Node(20)
 root.left.left=Node(30)
 root.right.right=Node(40)
 root.left.right=Node(45)
-#root.display()
-#root.inorder(root)
-#root.preorder(root)
+print("Normal Traversal")
+root.display()
+print("/n")
+print("Inorder Traversal")
+root.inorder(root)
+print("/n")
+print("Preorder Traversal")
+root.preorder(root)
+print("/n")
+print("Postorder Traversal")
 root.postorder(root)
